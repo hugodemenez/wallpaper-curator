0dd8edf57c02551b10447e11666ec0f8dd1db3f2
Hugo DEMENEZ <hdemenez@hotmail.fr>
2026-07-21 00:24:05 +0100

    Revert "Merge pull request #8 from hugodemenez/cursor/static-og-for-x-2790"
    This reverts commit a4a2a81f59802c472e6c154da66ad9866e79a620, reversing
    changes made to 8664ffbb425b1496f841e6043b67f5e95314292f.

---
+++ src/app/twitter-image.tsx
@@ -0,0 +1,16 @@
+import {
+  ogAlt,
+  ogContentType,
+  ogSize,
+  renderOgImage,
+} from "@/lib/og-response";
+
+export const alt = ogAlt;
+export const size = ogSize;
+export const contentType = ogContentType;
+export const runtime = "nodejs";
+export const revalidate = 86400;
+
+export default async function TwitterImage() {
+  return renderOgImage();
+}
